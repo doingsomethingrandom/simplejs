09aa9f95ea2894d13c7ea9781fe95fbf1f686a63
Tharun <tharunpandiyanv@gmail.com>
2021-02-09 17:04:23 +0000

    Just making it useable by adding export statement

--- out.js
+++ out.js
@@ -1,14 +1,15 @@
 function print(txt, q, by) {
-  if (q === undifined && !(by === ("QSall" || "QS"))) {
-    var err = "Error: you can't have the \"q\" field as undifined when you specify \"QSall\" or \"QS\" in the \"by\" field of \"out.print()\"";
+  if (q === undefined && !(by === ("QSall" || "QS"))) {
+    var err = "Error: you can't have the \"q\" field as undefined when you specify \"QSall\" or \"QS\" in the \"by\" field of \"out.print()\"";
     console.log(err);
-  } else if (by !== ("QS" || "QSall")) {
+  } else if (by !== ("QS" || "QSall") && !(by === undefined)) {
     var err = "Error: the \"by\" field of \"out.print()\" is not \"QS\" or \"QSall\" they are the only acceptable ones your code contains something different"
   } else if (by === "QS") {
     document.querySelector(q).innerHTML = txt;
   } else if (by === "QSall") {
     document.querySelectorAll(q).innerHTML = txt;
-  } else if (by === null) {
+  } else if (by === undefined && q === undefined) {
     document.write(txt);
   }
 }
+export print;
