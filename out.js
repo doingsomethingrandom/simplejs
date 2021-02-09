a3ea164ba271bc27dd7b01dc9ae6c3ecfd0e580b
Tharun <tharunpandiyanv@gmail.com>
2021-02-09 16:52:36 +0000

    Getting rid of js reserved words

--- out.js
+++ out.js
@@ -1,13 +1,13 @@
-function print(txt, ID, by) {
-  if (ID === undifined && !(by === ("QSall" || "QS"))) {
-    var err = "Error: you can't have the ID field as undifined when you specify QSall or QS in the by field of out.print()";
+function print(txt, Id, by) {
+  if (Id === undifined && !(by === ("QSall" || "QS"))) {
+    var err = "Error: you can't have the \"Id\" field as undifined when you specify \"QSall\" or \"QS\" in the \"by\" field of \"out.print()\"";
     console.log(err);
   } else if (by !== ("QS" || "QSall")) {
-    var err = "Error: the by field of out.print() is not QS or QSall they are the only acceptable ones your code contains something different"
+    var err = "Error: the \"by\" field of \"out.print()\" is not \"QS\" or \"QSall\" they are the only acceptable ones your code contains something different"
   } else if (by === "QS") {
-    document.querySelector(ID);
+    document.querySelector(Id).innerHTML = txt;
   } else if (by === "QSall") {
-    document.querySelectorAll(ID).innerHTML = txt;
+    document.querySelectorAll(Id).innerHTML = txt;
   } else if (by === null) {
     document.write(txt);
   }
