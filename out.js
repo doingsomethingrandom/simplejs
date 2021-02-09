c6c0f3652437eb51b1c75a22e8158023b738ed72
Tharun <tharunpandiyanv@gmail.com>
2021-02-09 16:53:54 +0000

    No more reserved words once and for all

--- out.js
+++ out.js
@@ -1,13 +1,13 @@
-function print(txt, Id, by) {
-  if (Id === undifined && !(by === ("QSall" || "QS"))) {
-    var err = "Error: you can't have the \"Id\" field as undifined when you specify \"QSall\" or \"QS\" in the \"by\" field of \"out.print()\"";
+function print(txt, q, by) {
+  if (q === undifined && !(by === ("QSall" || "QS"))) {
+    var err = "Error: you can't have the \"q\" field as undifined when you specify \"QSall\" or \"QS\" in the \"by\" field of \"out.print()\"";
     console.log(err);
   } else if (by !== ("QS" || "QSall")) {
     var err = "Error: the \"by\" field of \"out.print()\" is not \"QS\" or \"QSall\" they are the only acceptable ones your code contains something different"
   } else if (by === "QS") {
-    document.querySelector(Id).innerHTML = txt;
+    document.querySelector(q).innerHTML = txt;
   } else if (by === "QSall") {
-    document.querySelectorAll(Id).innerHTML = txt;
+    document.querySelectorAll(q).innerHTML = txt;
   } else if (by === null) {
     document.write(txt);
   }
